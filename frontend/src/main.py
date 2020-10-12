4db47920d78e4f4b96f3a26163c2016374859877
Henry Diep <diephenry95@gmail.com>
2020-10-11 20:34:53 -0700

    Edit connections in Dockerfiles & frontend

--- frontend/src/main.py
+++ frontend/src/main.py
@@ -13,7 +13,7 @@ issue_age = st.number_input('Issue Age', 18, 80, 30)
 bmi = st.number_input('BMI', 10.0, 50.0, 24.5, 0.1)
 
 if st.button('Submit'):
-    url = 'http://10.184.26.9:80/predict'
+    url = 'http://fastapi:8000/predict'
     gender_binary = 0
     if gender == 'Female':
         gender_binary = 1
